fe974c2ce911b07ba3435b6f580a9331e88f287e
seth1025 <seth1025@byu.edu>
2024-02-24 19:05:41 -0700

    GameData record

---
+++ shared/src/main/java/model/GameData.java
@@ -0,0 +1,10 @@
+package model;
+import chess.ChessGame;
+
+public record GameData(int gameID, String whiteUsername, String blackUsername, String gameName, ChessGame game) {
+    public GameData setPlayer(String userName, ChessGame.TeamColor color) {
+        if (color == ChessGame.TeamColor.WHITE) return new GameData(gameID, userName, blackUsername, gameName, game);
+        else return new GameData(gameID, whiteUsername, userName, gameName, game);
+    }
+}
+
