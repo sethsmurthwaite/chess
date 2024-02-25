c85400598c4870a24626f6cf3c2f29bb74d65403
seth1025 <seth1025@byu.edu>
2024-02-24 19:02:50 -0700

    added some stuff to DataAccessException

--- server/src/main/java/dataAccess/DataAccessException.java
+++ server/src/main/java/dataAccess/DataAccessException.java
@@ -4,6 +4,8 @@ package dataAccess;
  * Indicates there was an error connecting to the database
  */
 public class DataAccessException extends Exception{
+    int code;
+
     public DataAccessException(String message) {
         super(message);
     }
