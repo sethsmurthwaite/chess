69d6d0792046e15f5abac548441925891df74ae3
Seth Smurthwaite <seth.smurthwaite@gmail.com>
2024-03-06 08:34:30 -0700

    making progress

--- server/src/main/java/dataAccess/AuthDAO.java
+++ server/src/main/java/dataAccess/AuthDAO.java
@@ -2,30 +2,10 @@ package dataAccess;
 
 import model.AuthData;
 
-import java.util.HashMap;
-import java.util.UUID;
+public interface AuthDAO {
+    public AuthData createAuth(String username) throws DataAccessException;
+    public void clearAuth();
+    public void deleteAuth(String authToken);
+    public AuthData readAuth(String authToken) throws DataAccessException;
 
-public class AuthDAO {
-
-    static HashMap<String, String> authTable = new HashMap<String, String>();
-
-    public AuthData createAuth(String username) throws DataAccessException {
-        String authToken = UUID.randomUUID().toString();
-        this.authTable.put(authToken, username);
-        return new AuthData(authToken, username);
-    }
-
-    public void clearAuth() {
-        authTable.clear();
-    }
-
-    public void deleteAuth(String authToken) {
-        authTable.remove(authToken);
-    }
-
-    public AuthData readAuth(String authToken) throws DataAccessException {
-        String username = authTable.get(authToken);
-        if (username == null) throw new DataAccessException("Bad Auth Token", 401);
-        return new AuthData(authToken, username);
-    }
 }
