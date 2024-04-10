d7757c591ed686249902f104b45e91b200b8cf8f
Seth Smurthwaite <seth.smurthwaite@gmail.com>
2024-04-09 18:25:12 -0600

    make some progress

---
+++ shared/src/main/java/webSocketMessages/userCommands/Resign.java
@@ -0,0 +1,14 @@
+package webSocketMessages.userCommands;
+
+public class Resign extends UserGameCommand {
+    int gameID;
+
+    public Resign(String authToken, int gameID) {
+        super(authToken, CommandType.RESIGN);
+        this.gameID = gameID;
+    }
+
+    public int getGameID() {
+        return gameID;
+    }
+}
